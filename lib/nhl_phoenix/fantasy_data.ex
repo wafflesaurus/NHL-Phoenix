ba12484f22d6e84a4dc08d1351009240ae66bc67
wafflesaurus <ericbrusven@yahoo.com>
2017-03-01 23:40:43 -0600

    Add calls to API

--- lib/nhl_phoenix/fantasy_data.ex
+++ lib/nhl_phoenix/fantasy_data.ex
@@ -10,8 +10,18 @@ defmodule NhlPhoenix.FantasyWrapper do
     api_call(url)
   end
 
+	def get_box_score_delta(date) do
+    url = "https://api.fantasydata.net/nhl/v2/json/BoxScoresDelta/#{date}"
+    api_call(url)
+  end
+
 	def get_box_score(game_id) do
     url = "https://api.fantasydata.net/nhl/v2/json/BoxScore/#{game_id}"
+    api_call(url)
+  end
+
+	def get_player_stats_by_team(season, team) do
+    url = "https://api.fantasydata.net/nhl/v2/json/BoxScore/#{season}/#{team}"
     api_call(url)
   end
 
